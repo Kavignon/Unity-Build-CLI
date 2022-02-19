7203224ca38a7d56bc6e81933e0ccf7a5ae6a8bb
Kevin Avignon <kevin.avignon5@gmail.com>
2022-02-19 18:05:37 -0500

    Implemented the CLI command allowing to build a player
    We have here an infrastructure that can initially check whether the provided build target is among the list of targets that Unity supports before setting it.
    
    Afterwards, we'll try to use the script file in the game project which holds the logic on how to build the player. In this case, I've chosen to go with Android and updated the project locally by adding a script over there thanks to the documentation at
    
    https://docs.unity3d.com/Manual/EditorCommandLineArguments.html?fbclid=IwAR1HYTOhBBkJ838A9bxNtYt4mNqkT0IeRB5OwCCbXUjVnSOnAooM75xKPs4
    
    (Examples)

--- src/Unity.Build.CLI/Program.cs
+++ src/Unity.Build.CLI/Program.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using CommandLine;
 using Unity.Build.CLI.ArgumentParsing;
+using Unity.Build.CLI.Infrastructure;
 
 namespace Unity.Build.CLI
 {
-    internal class Program
+    internal sealed class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Parser.Default.ParseArguments<CloneOptions>(args)
+            Parser.Default.ParseArguments<CloneOptions, GameBuildOptions>(args)
                 .WithParsed<CloneOptions>(options =>
                 {
                     ScmInfrastructure.SetGitDefaultBranchName();
                     ScmInfrastructure.CloneFromGit(options.ProjectUrl, options.DirectoryPath, options.SubDirectoryPath);
+                })
+                .WithParsed<GameBuildOptions>(options =>
+                {
+                    UnityEditorInfrastructure.SetActiveBuildTarget(options.BuildTarget);
+                    UnityEditorInfrastructure.BuildPlayer(options.UnityProjectLocalPath, options.ScriptFileName, options.BuildPlayerMethodName);
                 })
                 .WithNotParsed(_ => Console.WriteLine("An error occurred while trying to parse the given user input."));
         }
