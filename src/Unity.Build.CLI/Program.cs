a633462cd1e111c61f9f18730f7f894a3ebf9273
Kevin Avignon <kevin.avignon5@gmail.com>
2022-02-19 15:43:51 -0500

    Implemented first-pass for the repository cloning feature.
    Currently, the clone feature needed the name of the default branch when pulling the project from the GitHub link. To avoid having to make this a problem for others developers if they have a Git version of >=2.28, I made it something the tool can handle.
    
    For speed, we're focusing right now on downloading the repository from HTTPS. Furthermore, here, we've only taken care of the happy path. We don't display anything to the user yet in case of failure such as the provided link doesn't exist, or they don't have the required access to see the link they provided or even, they're trying to clone the repository to the same location it's been downloaded.
    
    When cloning the repository, the developer will need to provide the name of the folder on which we'll copy the contents from the repository.

--- src/Unity.Build.CLI/Program.cs
+++ src/Unity.Build.CLI/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+// We're taking for granted that Git is installed on the developer's machine as a SCM.
+const string ScmUseByCli = "git";
+
+static void CloneFromGit(string projectUrl, string? directoryPath, string localDirectoryName)
+{
+    if (string.IsNullOrEmpty(projectUrl))
+    {
+        // TODO: Let's add logs for when we have interesting things to tell the developer.
+        
+        Console.WriteLine("We cannot move forward if the project's URL is missing.");
+    }
+
+    // When the directory path isn't provided, the tool will try to clone the repository at the current local from where the EXE was invoked.
+    directoryPath = CanCloneToLocal(directoryPath) ? Path.Combine(directoryPath, localDirectoryName) : string.Empty;
+
+    ExecuteScmCommand(ScmUseByCli, $"clone {projectUrl} {directoryPath}");
+
+    static bool CanCloneToLocal(string? directoryPath) => !string.IsNullOrEmpty(directoryPath) && !Directory.Exists(directoryPath);
+}
+
+static void SetGitDefaultBranchName(string? branchName)
+{
+    string defaultName = branchName ?? "master";
+    string command = $"config --global init.defaultBranch {defaultName}";
+
+    ExecuteScmCommand(ScmUseByCli, command);
+}
+
+static void ExecuteScmCommand(string scmName, string command, int processWaitTime = 10_000)
+{
+    var process = new Process
+    {
+        StartInfo = new ProcessStartInfo
+        {
+            FileName = $"{scmName}",
+            Arguments = command,
+            WindowStyle = ProcessWindowStyle.Hidden
+        }
+    };
+
+    process.Start();
+    process.WaitForExit(processWaitTime);
+}
+
+// Fixes a problem that arises when Git version 2.28+ is installed - https://stackoverflow.com/questions/64349920/git-error-fatal-invalid-branch-name-init-defaultbranch
+SetGitDefaultBranchName(null);
+CloneFromGit("https://github.com/wrossmck/2d-platformer/", "C:\\sources/2d-platformer", "2d-platformer");
